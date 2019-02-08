46388eab0e9b19dc9c6ebafe563b6d8179a63e07
jz222cv <38494528+jz222cv@users.noreply.github.com>
2019-02-09 02:33:02 +0800

    Add files via upload

---
+++ code/src/model/Player.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Player {
+	
+    private String name;
+
+    public Player(String playerName) {
+        name = playerName;
+    }
+    
+    public String getName() {
+    	return name;
+    }
+    
+}
+
